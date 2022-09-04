c1961c312136ecae07f8c1b6871abeccf69b4bb5
vismate <visnyarmate@gmail.com>
2022-09-04 21:13:53 +0200

    adapted dotfiles for desktop

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -16,7 +16,7 @@ require('packer').startup( function()
     -- Useful completion sources:
     use 'hrsh7th/cmp-nvim-lua'
     use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'                             
+    -- use 'hrsh7th/cmp-vsnip'                             
     use 'hrsh7th/cmp-path'                              
     use 'hrsh7th/cmp-buffer'                            
     use 'hrsh7th/vim-vsnip'
