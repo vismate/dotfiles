df44fa8286b7958a544b56157f092efd2f452b66
vismate <visnyarmate@gmail.com>
2022-09-04 13:47:31 +0200

    nvim conf changes

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -36,12 +36,10 @@ require('packer').startup( function()
       requires = "nvim-lua/plenary.nvim",
       config = function()
         require("todo-comments").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-        end
-     }
+          
+            
+        } end
+    }
     
     use {
       "folke/trouble.nvim",
